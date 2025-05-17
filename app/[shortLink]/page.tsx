b4fb7e1ce9d86956993f3bd6f7b61dea8d24f14b
Cesar Freire <iceesar@live.com>
2025-05-17 19:30:53 -0300

    fix: 404 if url dont exists

--- app/[shortLink]/page.tsx
+++ app/[shortLink]/page.tsx
@@ -1,6 +1,6 @@
 import { ShortlinkParam } from "@/types/url.types";
 import clientPromise from "@/lib/mongodb";
-import { redirect } from "next/navigation";
+import { redirect, notFound } from "next/navigation";
 
 export default async function RedirectShortlinkPage({
   params,
@@ -19,47 +19,16 @@ export default async function RedirectShortlinkPage({
     .collection("links")
     .findOne({ shortUrl: shortlinkUrl });
 
-  // If the shortlink exists, redirect to the original url
-  if (shortlink) {
-    const originalUrl = shortlink.url;
-
-    // Redirect to the original url
-    if (originalUrl) {
-      // Increment the access count
-      await db
-        .collection("links")
-        .updateOne({ shortUrl: shortlinkUrl }, { $inc: { accessCount: 1 } });
-      // Redirect to the original url
-      redirect(originalUrl);
-    } else {
-      return (
-        <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-          <h1 className="text-2xl font-bold">Shortlink not found.</h1>
-        </div>
-      );
-    }
+  // If the shortlink does not exist, return a 404
+  if (!shortlink || !shortlink.url) {
+    notFound();
   }
-  return (
-    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <h1 className="text-2xl font-bold">Shortlink not found.</h1>
-      <p className="text-lg text-muted-foreground text-center max-w-md">
-        The shortlink you tried to access does not exist or has expired. Create
-        a new link and share it with your friends!
-      </p>
-      <p className="text-sm text-muted-foreground">
-        You will be redirected to the home page.
-      </p>
 
-      {/* Redirecionamento automático para a home */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            setTimeout(() => {
-              window.location.href = "/";
-            }, 5000);
-          `,
-        }}
-      />
-    </div>
-  );
+  // Increment the access count
+  await db
+    .collection("links")
+    .updateOne({ shortUrl: shortlinkUrl }, { $inc: { accessCount: 1 } });
+
+  // Redirect to the original URL
+  redirect(shortlink.url);
 }
